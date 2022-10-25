c917022e62ed31328b1ecd13e37bb338bea4f6c0
Ksenia <ladoshkoksenia@gmail.com>
2022-10-25 10:54:05 +0300

    3

--- app/src/main/java/com/ksusha/travely/ui/fragments/DetailsFragment.kt
+++ app/src/main/java/com/ksusha/travely/ui/fragments/DetailsFragment.kt
@@ -6,13 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.ksusha.travely.data.Attraction
 import com.ksusha.travely.databinding.FragmentDetailsBinding
+import com.squareup.picasso.Picasso
 
-class DetailsFragment : Fragment() {
+class DetailsFragment : BaseFragment() {
 
     private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding!!
     private val safeArgs: DetailsFragmentArgs by navArgs()
+    private val attraction: Attraction by lazy {
+        attractions.find { it.id == safeArgs.attractionId }!!
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,7 +30,14 @@ class DetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.button.text = safeArgs.attractionId
+        binding.titleTextView.text = attraction.title
+        binding.descriptionTextView.text = attraction.description
+        Picasso.get().load(attraction.image_url).into(binding.headerImageView)
+        binding.monthsToVisitTextView.text = attraction.months_to_visit
+        binding.numberOfFactsTextView.text = "${attraction.facts.size} facts"
+        binding.numberOfFactsTextView.setOnClickListener {
+
+        }
     }
 
     override fun onDestroyView() {
